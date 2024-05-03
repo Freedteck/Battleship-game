ad1ee2ab3f800f69f13a958ac9193148ed97872d
Freedteck <freedteck@gmail.com>
2024-05-03 10:40:32 +0100

    Fix error and Add features
    fix errors of infinite recursion and add fetures to show modals when starting and when game over

--- Game.js
+++ Game.js
@@ -36,18 +36,16 @@ const Game = () => {
   const cGameBoard = document.createElement("div");
   cGameBoard.classList.add("c-game-board");
 
-  const randomize = document.createElement('button')
-  randomize.textContent = 'Randomize'
+  const randomize = document.createElement("button");
+  randomize.textContent = "Randomize";
 
   const players = Player();
   const player = players.player;
   const computer = players.computer;
 
   const existed = [];
-  let gameOver = false;
   let playerTurn = true;
-
-  randomize.style.display = gameOver ? 'none' : 'block'
+  randomize.style.display = "none";
 
   const gameLoop = () => {
     const placePlayerShips = () => {
@@ -132,105 +130,145 @@ const Game = () => {
   };
 
   const resetBoard = () => {
-
+    // Clear both player's and computer's boards
     for (let i = 0; i < player.board.length; i++) {
       for (let j = 0; j < player.board[i].length; j++) {
-
-        if (player.board[i][j]) {
-          player.board[i][j] = null
-        }
+        player.board[i][j] = null;
+        computer.board[i][j] = null;
       }
     }
 
-    for (let i = 0; i < computer.board.length; i++) {
-      for (let j = 0; j < computer.board[i].length; j++) {
+    // Reinitialize the game
+    gameLoop();
 
-        if (computer.board[i][j]) {
-          computer.board[i][j] = null
-        }
-      }
-    }
-
-    gameLoop()
-    // Clear player's board
-    gameOver = false;
+    // Clear and render player's board
     pGameBoard.innerHTML = "";
     renderPlayerBoard(pGameBoard);
 
+    // Clear and render computer's board
     cGameBoard.innerHTML = "";
     renderComputerBoard(cGameBoard);
   };
 
-  const computerAttack = () => {
-    let row, col;
+  const gameOverModal = (player) => {
+    const dialogContainer = document.createElement("dialog");
+    const dialogText = document.createElement("p");
+    dialogText.textContent =
+      player === "Computer"
+        ? "This is not a joke! The computer WIN!!"
+        : "WOW!! Congratulations! You WIn the Challenge";
+    const okay = document.createElement("button");
+    okay.textContent = "Okay";
+    dialogContainer.appendChild(dialogText);
+    dialogContainer.appendChild(okay);
+    main.appendChild(dialogContainer);
 
-    // Generate random coordinates until finding an unattacked position
-    do {
-      row = Math.floor(Math.random() * 10);
-      col = Math.floor(Math.random() * 10);
-    } while (existed.some((coord) => coord[0] === row && coord[1] === col));
-
-    if (!gameOver) {
-      // Attack player's ships
-      players.attackShip(player, row, col);
-
-      // Record the attacked position
-      existed.push([row, col]);
-
-      // Update the UI to reflect the attack
-      const cells = pGameBoard.querySelectorAll(".cell");
-      cells.forEach((cell) => {
-        if (cell.dataset.id === row.toString() && cell.id === col.toString()) {
-          cell.classList.add("clicked");
-          if (player.board[row][col] && player.board[row][col].isSunk()) {
-            cell.textContent = "X";
-            cell.classList.remove("ship");
-            cell.classList.add("hit");
-            setTimeout(() => {
-              computerAttack();
-            }, 1000);
-          } else {
-            cell.style.backgroundColor = "grey";
-            playerTurn = true;
-            toast.textContent = "Your turn";
-          }
-        }
-      });
-    }
-    if (player.allShipSunk()) {
-      gameOver = true;
-      alert("Computer Won");
+    okay.addEventListener("click", () => {
       setTimeout(() => {
         resetBoard();
       }, 1000);
+      dialogContainer.close();
+      main.removeChild(dialogContainer);
+    });
+    return dialogContainer;
+  };
+
+  const computerAttack = (row, col) => {
+    // let row, col;
+
+    if (!row && !col) {
+      // Generate random coordinates until finding an unattacked position
+      do {
+        row = Math.floor(Math.random() * 10);
+        col = Math.floor(Math.random() * 10);
+      } while (existed.some((coord) => coord[0] === row && coord[1] === col));
     }
+
+    // if (!gameOver) {
+    // Attack player's ships
+    players.attackShip(player, row, col);
+
+    // Record the attacked position
+    existed.push([row, col]);
+
+    // Update the UI to reflect the attack
+    const cells = pGameBoard.querySelectorAll(".cell");
+    cells.forEach((cell) => {
+      if (cell.dataset.id === row.toString() && cell.id === col.toString()) {
+        cell.classList.add("clicked");
+        if (
+          player.board[row] &&
+          player.board[row][col] &&
+          player.board[row][col].isSunk()
+        ) {
+          cell.textContent = "X";
+          cell.classList.remove("ship");
+          cell.classList.add("hit");
+          player.board[row][col] = null;
+          playerTurn = false
+          if (player.allShipSunk()) {
+            playerTurn = true;
+            gameOverModal(computerHead.textContent).showModal();
+          }
+          setTimeout(() => {
+            if (row > 0 && row < 9) {
+              if (
+                player.board[row - 1][col] &&
+                !player.board[row - 1][col].isSunk()
+              ) {
+                computerAttack(row - 1, col);
+              } else if (
+                player.board[row + 1][col] &&
+                !player.board[row + 1][col].isSunk()
+              ) {
+                computerAttack(row + 1, col);
+              } else if (
+                player.board[row][col - 1] &&
+                !player.board[row][col - 1].isSunk()
+              ) {
+                computerAttack(row, col - 1);
+              } else if (
+                player.board[row][col + 1] &&
+                !player.board[row][col + 1].isSunk()
+              ) {
+                computerAttack(row, col + 1);
+              } else {
+                computerAttack();
+              }
+            } else {
+              computerAttack();
+            }
+          }, 1000);
+        } else {
+          cell.style.backgroundColor = "grey";
+          playerTurn = true;
+          toast.textContent = "Your turn";
+        }
+      }
+    });
   };
 
   const playerAttack = (cell, row, col) => {
     const cells = pGameBoard.querySelectorAll(".cell");
-    if (!gameOver) {
-      players.attackShip(computer, row, col);
-      cell.classList.add("clicked");
-      if (computer.board[row][col] && computer.board[row][col].isSunk()) {
-        cell.textContent = "X";
-        cell.classList.remove("ship");
-        cell.classList.add("hit");
-      } else {
-        cell.style.backgroundColor = "grey";
-        playerTurn = false;
-        toast.textContent = "Opponent turn";
-        setTimeout(() => {
-          computerAttack();
-        }, 1000);
-      }
-    }
-    if (computer.allShipSunk()) {
-      gameOver = true;
-      alert("You Won");
-      playerTurn = true;
+    // if (!gameOver) {
+    players.attackShip(computer, row, col);
+    cell.classList.add("clicked");
+    if (computer.board[row][col] && computer.board[row][col].isSunk()) {
+      cell.textContent = "X";
+      cell.classList.remove("ship");
+      cell.classList.add("hit");
+    } else {
+      cell.style.backgroundColor = "grey";
+      toast.textContent = "Opponent turn";
+      playerTurn = false
       setTimeout(() => {
-        resetBoard();
+        computerAttack();
       }, 1000);
+    }
+    // }
+    if (computer.allShipSunk()) {
+      playerTurn = true;
+      gameOverModal(playerHead.textContent).showModal();
     }
   };
 
@@ -247,16 +285,56 @@ const Game = () => {
       container.appendChild(computerBoardContainer);
       toast.textContent = "Your turn";
     })();
+    randomize.style.display = "flex";
   }, 1500);
 
-  randomize.addEventListener('click', (e) => {
+  randomize.addEventListener("click", (e) => {
     console.log(e.target);
-    resetBoard()
-  })
+    resetBoard();
+  });
   main.appendChild(toastContainer);
   main.appendChild(container);
   main.appendChild(randomize);
   document.body.appendChild(main);
 };
 
-Game();
+const createGameModal = () => {
+  const modal = document.createElement("div");
+  modal.classList.add("modal");
+
+  const modalContent = document.createElement("div");
+  modalContent.classList.add("modal-content");
+
+  const greetings = document.createElement("h1");
+  greetings.textContent = "Welcome to Battleship!";
+  const rules = document.createElement("p");
+  rules.innerHTML = `<ul>
+  <li>
+  The objective of the game is to sink all of your opponent's ships before
+  they sink yours.
+</li>
+<li>Each player places their ships on their board,</li>
+<li>
+  And then takes turns attacking the opponent's board by guessing the
+  coordinates of their ships.
+</li>
+</ul>`;
+
+  const startButton = document.createElement("button");
+  startButton.textContent = "Start";
+
+  startButton.addEventListener("click", () => {
+    modal.style.display = "none"; // Hide modal
+    Game(); // Start the game
+  });
+
+  modalContent.appendChild(greetings);
+  modalContent.appendChild(rules);
+  modalContent.appendChild(startButton);
+  modal.appendChild(modalContent);
+
+  document.body.appendChild(modal);
+
+  return modal;
+};
+createGameModal();
